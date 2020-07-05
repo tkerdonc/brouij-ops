1b197023bfa0402529c3b818420d08fecfc1ef4e
tkerdonc <t.kerdonc@gmail.com>
2020-07-05 17:40:52 +0200

    Add public gallery

--- server/resources/nginx/nginx_entrypoint.sh
+++ server/resources/nginx/nginx_entrypoint.sh
@@ -4,4 +4,5 @@ set -x
 sed -e "s/SERVER_NAME/${SERVER_NAME}/g" < /etc/nginx/conf.d/default.template \
                                         > /etc/nginx/conf.d/default.conf
 
+
 nginx -g "daemon off;"
